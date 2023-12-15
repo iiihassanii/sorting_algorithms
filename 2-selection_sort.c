a8bdaf903244df49735d567bb51fd42b498ee67f
mazinmohd <mazenomermazen2002@gmail.com>
2023-12-15 13:02:50 +0300

    edit on betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,9 @@
 #include "sort.h"
-
+/**
+  * selection_sort - sort an array of integers
+  * @array: the array we want to sort
+  * @size: the size of array
+ */
 
 void selection_sort(int *array, size_t size)
 {
@@ -16,7 +20,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[smallest])
 				smallest = j;
-		
+
 		if (smallest != i)
 		{
 			tmp = array[i];
